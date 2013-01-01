7c785c1ee1c8987452585171c3226658d1265a97
Eyal Lotem <eyal.lotem@gmail.com>
2013-01-01 17:55:21 +0200

    fprintf to stderr that msync is happening

--- cbits/posix.c
+++ cbits/posix.c
@@ -4,6 +4,7 @@
 #define _FILE_OFFSET_BITS 64
 
 #include <sys/mman.h>
+#include <stdio.h>
 
 int system_io_msync(void *ptr, size_t size, int hs_flags)
 {
@@ -17,5 +18,6 @@ int system_io_msync(void *ptr, size_t size, int hs_flags)
     if (hs_flags & 4) {
         flags |= MS_INVALIDATE;
     }
+    fprintf(stderr, "msync: %p, %zu, flags=0x%X (hs_flags=0x%X)\n", ptr, size, flags, hs_flags);
     return msync(ptr, size, flags);
 }
